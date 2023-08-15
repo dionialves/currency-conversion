75845d1ac70902c3ebf797c00aaaac940020accf
dionialves <dionialves@gmail.com>
2023-08-14 21:32:06 -0300

    Implemented restfull awesomeapi API consumption

--- javascript.js
+++ javascript.js
@@ -1,22 +1,19 @@
-
 const convertFrom = document.getElementById("convertFrom");
 const convertTo = document.getElementById("convertTo");
-const value = document.getElementById("value");
-const buttonConverter = document.getElementById("buttonConverter");
 const imgConvertFrom = document.getElementById("imgConvertFrom");
 const imgConvertTo = document.getElementById("imgConvertTo");
 const textCoinFrom = document.getElementById("textCoinFrom");
 const textCoinTo = document.getElementById("textCoinTo");
 const valueCoinFrom = document.getElementById("valueCoinFrom");
 const valueCoinTo = document.getElementById("valueCoinTo");
+const buttonConverter = document.getElementById("buttonConverter");
 
-let oldValueInSelectFrom = 0;
-let oldValueInSelectTo = 1;
 
-// mudar select para texto
-//
+let oldValueInSelectFrom = "real";
+let oldValueInSelectTo = "dolar";
 
-const coin = {
+// Lista de moedas disponíveis para converção
+const coins = {
     dolar: {
         code: "US$",
         name: "Dolar Americano",
@@ -31,15 +28,15 @@ const coin = {
         pathImage: "./img/real.png",
         languageCode: 'pt-BR',
         currency: 'BRL',
-        conversionRate: 4.75
+        conversionRate: 0
     },
     euro: {
         code: "€",
         name: "Euro",
         pathImage: "./img/euro.png",
         languageCode: 'en-GB',
-        currency: 'GBP',
-        conversionRate: 0.91
+        currency: 'EUR',
+        conversionRate: 0
     },
     libra: {
         code: "£",
@@ -47,118 +44,56 @@ const coin = {
         pathImage: "./img/libra.png",
         languageCode: 'en-GB',
         currency: 'GBP',
-        conversionRate: 0.78
+        conversionRate: 0
     },
-    bitcoin: {
-        code: "₿",
-        name: "Bitcon",
-        pathImage: "./img/bitcoin.png",
-        languageCode: 'en-US',
-        currency: 'BTC',
-        conversionRate: 0.000034
+};
+
+const changeValueOnSelect = () => {
+    // Verifica se o são diferente, caso sim, altera as informações de 
+    // imagem, texto e valor
+    // Caso são iguais inverte os valores
+    if (convertFrom.value != convertTo.value) {
+        imgConvertFrom.src = coins[convertFrom.value].pathImage
+        textCoinFrom.textContent = coins[convertFrom.value].name
+        //valueCoinFrom.textContent = coins[convertFrom.value].code + " 0,00";
+
+        imgConvertTo.src = coins[convertTo.value].pathImage
+        textCoinTo.textContent = coins[convertTo.value].name
+        //valueCoinTo.textContent = coins[convertTo.value].code + " 0,00";
+    } else {
+        convertFrom.value = oldValueInSelectTo;
+        convertTo.value = oldValueInSelectFrom;
+        changeValueOnSelect()
+
     }
+    // Atualiza as variáveis abaixo com os valores atuais do select.
+    // Essas variáveis são usadas como auxiliares
+    oldValueInSelectFrom = convertFrom.value;
+    oldValueInSelectTo = convertTo.value;
+
+    convertCoin()
 };
 
-
-const listOfCoins = Object.keys(coin);
-
-const listOfOptions = createListOfCoins(listOfCoins);
-const listImage = createListImageOfCoins(listOfCoins);
-
-addOptionsSelect(listOfOptions, convertFrom, 1);
-addOptionsSelect(listOfOptions, convertTo, 0);
-
-
-
-function createListOfCoins(CoinsInObjects){
-
-    list = [];
-    for (let i=0; i < CoinsInObjects.length; i++){
-        list.push(coin[CoinsInObjects[i]].code + " " + coin[CoinsInObjects[i]].name)
-    };
-
-    return list;
-};
-
-function createListImageOfCoins(CoinsInObjects){
-
-    list = [];
-    for (let i=0; i < CoinsInObjects.length; i++){
-        list.push(coin[CoinsInObjects[i]].pathImage);
-    }
-
-    return list;
-};
-
-function addOptionsSelect(list, select, initialValue){
-
-    for (let i=0; i < list.length; i++) {
+const convertCoin = async () => {
+    code = `${coins[convertFrom.value].currency}${coins[convertTo.value].currency}`
     
-        const option = document.createElement('option');
-        option.value = i;
-        option.text = list[i];
-    
-        select.appendChild(option);
-    };
-    select.value = initialValue;
-};
+    const data = await fetch(
+        `http://economia.awesomeapi.com.br/json/last/${coins[convertFrom.value].currency}-${coins[convertTo.value].currency}`
+        ).then(response => response.json())
 
-function changeValueFrom(){
-
-    if (convertFrom.selectedIndex == convertTo.selectedIndex) {
-        invertValuesBetweenFromTo();
-    }
-    oldValueInSelectFrom = convertFrom.selectedIndex;
-    oldValueInSelectTo = convertTo.selectedIndex;
-    updateResultConvertion();
-};
-
-function changeValueTo(){
-
-    if (convertFrom.selectedIndex == convertTo.selectedIndex) {
-        invertValuesBetweenFromTo();
-    }
-    oldValueInSelectFrom = convertFrom.selectedIndex;
-    oldValueInSelectTo = convertTo.selectedIndex;
-    updateResultConvertion();
-};
-
-function invertValuesBetweenFromTo(){
-    convertFrom.selectedIndex = oldValueInSelectTo;
-    convertTo.selectedIndex = oldValueInSelectFrom;
-
-    oldValueInSelectFrom = convertFrom.selectedIndex;
-    oldValueInSelectTo = convertTo.selectedIndex;
-};
-
-function updateResultConvertion(){
-    imgConvertFrom.src = listImage[convertFrom.selectedIndex];
-    textCoinFrom.textContent = listOfOptions[convertFrom.selectedIndex];
-    valueCoinFrom.textContent = coin[listOfCoins[convertFrom.selectedIndex]].code + " 0,00";
-
-    imgConvertTo.src = listImage[convertTo.selectedIndex];
-    textCoinTo.textContent = listOfOptions[convertTo.selectedIndex];
-    valueCoinTo.textContent = coin[listOfCoins[convertTo.selectedIndex]].code + " 0,00";
-};
-
-function convertCoin (){
-
-    result =  coin[listOfCoins[0]].conversionRate / coin[listOfCoins[convertFrom.selectedIndex]].conversionRate;
-    result = result.toFixed(2) * coin[listOfCoins[convertTo.selectedIndex]].conversionRate;
-
-    valueCoinFrom.textContent =  new Intl.NumberFormat(coin[listOfCoins[convertFrom.selectedIndex]].languageCode, {
+    valueCoinFrom.textContent =  new Intl.NumberFormat(coins[convertFrom.value].languageCode, {
         style: "currency",
-        currency: coin[listOfCoins[convertFrom.selectedIndex]].currency
+        currency: coins[convertFrom.value].currency
       }).format(value.value);
 
-    valueCoinTo.textContent = new Intl.NumberFormat(coin[listOfCoins[convertTo.selectedIndex]].languageCode, {
+    valueCoinTo.textContent = new Intl.NumberFormat(coins[convertTo.value].languageCode, {
         style: "currency",
-        currency: coin[listOfCoins[convertTo.selectedIndex]].currency,
+        currency: coins[convertTo.value].currency,
         maximumFractionDigits:8,
-      }).format(result * value.value)
+      }).format(data[code].high * value.value)
 
 }
 
-convertFrom.addEventListener('change', changeValueFrom);
-convertTo.addEventListener('change', changeValueTo);
+convertFrom.addEventListener('change', changeValueOnSelect);
+convertTo.addEventListener('change', changeValueOnSelect);
 buttonConverter.addEventListener('click', convertCoin);
